2828784a0b82725bca0fa6cdd9649fa87aed5ba6
Antonio Escorcia <packageinstaller2@gmail.com>
2021-05-19 20:32:15 -0600

    Clase de temporizador añadida
    Se ha añadido una clase de Temporizador muy facil de usar para manejar los eventos tiempo-dependientes dentro del juego, pueden revisar el script TemporizadorPrueba para ver rapidamente una implementacion

---
+++ Paleontojam_Game/Assets/Scripts/Temporizador.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Un temporizador que puede tener cualquier duracion
+/// </summary>
+public class Temporizador : MonoBehaviour
+{
+    #region Campos
+
+    // soporte de inicializacion
+    float segundosTranscurridos;
+    float segundosTotales;
+
+    // soporte de estados del temporizador
+    bool iniciado = false;
+    bool corriendo = false;
+
+    #endregion
+
+    #region Propiedades
+
+    /// <summary>
+    /// Permite definir la duracion del temporizador mientras no esté corriendo
+    /// </summary>
+    public float Duracion
+    {
+        set
+        {
+            if (!corriendo)
+            {
+                segundosTotales = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Permite conocer si el temporizador se ha finalizado
+    /// </summary>
+    public bool Finalizado
+    {
+        get
+        {
+            return iniciado && !corriendo;
+        }
+    }
+    #endregion
+
+    #region Metodos
+
+    /// <summary>
+    /// Inicia el temporizador si la duracion es mayor que 0, 
+    /// sirve tambien para reiniciar el temporizador
+    /// </summary>
+    public void Iniciar()
+    {
+        if(segundosTotales > 0)
+        {
+            iniciado = true;
+            corriendo = true;
+            segundosTranscurridos = 0;
+        }
+    }
+
+
+    /// <summary>
+    /// Actualiza el tiempo transcurrido en el temporizador
+    /// </summary>
+    void Update()
+    {
+        if (corriendo)
+        {
+            segundosTranscurridos += Time.deltaTime;
+            if (segundosTranscurridos > segundosTotales)
+            {
+                corriendo = false;
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Paleontojam_Game/Assets/Scripts/TemporizadorPrueba.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Prueba demostrativa del temporizador
+/// </summary>
+public class TemporizadorPrueba : MonoBehaviour
+{
+    // soporte para temporizador
+    Temporizador prueba;
+
+    /// <summary>
+    /// inicializa el temporizador
+    /// </summary>
+    void Start()
+    {
+        // instanciando temporizador
+        prueba = gameObject.AddComponent<Temporizador>();
+        prueba.Duracion = 1;
+        prueba.Iniciar();
+    }
+
+    /// <summary>
+    /// Resolviendo finalizacion del temporizador
+    /// </summary>
+    void Update()
+    {
+        if (prueba.Finalizado)
+        {
+            print("Se ha finalizado el temporizador");
+
+            // reiniciando
+            prueba.Iniciar();
+        }
+    }
+}
