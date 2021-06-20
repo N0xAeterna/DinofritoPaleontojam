69fe245f554703d49b05b3be18ae53dc15375ffb
Luis Johannsen <l.johannsen@getaclub.io>
2021-06-20 02:27:50 -0500

    Switch to URP and added cursor that aligns to normal
    Added a new indicator cursor that aligns with the surface normal.
    TODO: Deform mesh along the cursor's hitpoint with some sort of interpolation

---
+++ Paleontojam_Game/Assets/IndicatorFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorFollow : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField]
+    LayerMask layermask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CursorToWorldPosition();
+    }
+
+
+    void CursorToWorldPosition()
+    {
+        Ray rayToCameraPos = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        
+        if (Physics.Raycast(rayToCameraPos, out hitInfo, 100, layermask))
+        {
+            transform.position = hitInfo.point;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+    }
+}
