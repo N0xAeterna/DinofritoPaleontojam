983826a84890651df2edb082654bd892b80d23cc
Cytanic <maxvalentino@hotmail.com>
2021-06-22 11:06:51 -0500

    Menu , Trofeos y proximamente musicas

---
+++ Paleontojam_Game/Assets/Scenes/Scripts/Datos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Datos 
+{
+    public int NumOfLose;
+    public Datos (int numLose)
+    {
+        NumOfLose = numLose;
+    }
+ 
+}
