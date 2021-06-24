a231b190d9283f5d9dcbec5c3a08d68ec78b75f1
Cytanic <maxvalentino@hotmail.com>
2021-06-23 20:41:35 -0500

    Menu

--- Paleontojam_Game/Assets/Scripts/Audio/AudioManager.cs
+++ Paleontojam_Game/Assets/Scripts/Audio/AudioManager.cs
@@ -85,6 +85,7 @@ public static class AudioManager
                         {
                             if (!soundtrackAudioSource.isPlaying)
                                 soundtrackAudioSource.Play();
+                          
 
                             playing = true;
                         }
