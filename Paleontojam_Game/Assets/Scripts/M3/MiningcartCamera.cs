ee6f844c0ade029d0a5c7c1633370ac3d8c9ecf2
Antonio Escorcia <packageinstaller2@gmail.com>
2021-06-25 18:39:11 -0600

    Muchos cambios al minijuego 3
    Ya no se puede saltar ni rotar, simplemente va a transportar el bochon de izquierda a derecha y ya

--- Paleontojam_Game/Assets/Scripts/M3/MiningcartCamera.cs
+++ Paleontojam_Game/Assets/Scripts/M3/MiningcartCamera.cs
@@ -6,6 +6,7 @@
 public class MiningcartCamera : MonoBehaviour
 {
     GameObject player;
+    public static bool seguir = false;
 
     private void Start()
     {
@@ -17,11 +18,15 @@ public class MiningcartCamera : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        Vector3 newPosition = new Vector3();
-        newPosition.x = player.transform.position.x + 4;
-        newPosition.y = player.transform.position.y + 5;
-        newPosition.z = player.transform.position.z - 15f;
+        if(seguir)
+        {
+            Vector3 newPosition = new Vector3();
+            newPosition.x = player.transform.position.x + 6;
+            newPosition.y = player.transform.position.y + 17.93f;
+            newPosition.z = player.transform.position.z - 53.96f;
 
-        transform.position = newPosition;
+            transform.position = newPosition;
+        }
+        
     }
 }
