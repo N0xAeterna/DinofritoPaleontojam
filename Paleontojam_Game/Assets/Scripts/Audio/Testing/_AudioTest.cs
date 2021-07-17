eb0de77983d2ef5f30e8568ac4d9ca0461f22386
Antonio Escorcia <packageinstaller2@gmail.com>
2021-07-17 14:35:02 -0600

    Escena para probar audio
    Se agrego una escena para hacer prueba de audio, se va a implementar el fade in y el fade out

---
+++ Paleontojam_Game/Assets/Scripts/Audio/Testing/_AudioTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _AudioTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            AudioManager.StopAllSoundtracks();
+            AudioManager.PlaySoundtrack(AudioClipName.MinigameOneSoundtrack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            AudioManager.StopAllSoundtracks();
+            AudioManager.PlaySoundtrack(AudioClipName.MinigameTwoSoundtrack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            AudioManager.StopAllSoundtracks();
+            AudioManager.PlaySoundtrack(AudioClipName.MinigameThreeSoundtrack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.StopAllSoundtracks();
+        }
+    }
+}
