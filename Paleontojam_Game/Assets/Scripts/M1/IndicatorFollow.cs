c49fcf62a29588facad55a989f4c4e188dd47171
Manu-alfk <73673602+Manu-alfk@users.noreply.github.com>
2021-07-20 23:23:54 -0600

    cambios en deteccion de huesos

--- Paleontojam_Game/Assets/Scripts/M1/IndicatorFollow.cs
+++ Paleontojam_Game/Assets/Scripts/M1/IndicatorFollow.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-public class IndicatorFollow : MonoBehaviour
-{
+public class IndicatorFollow : MonoBehaviour {
     private Camera cam;
     [SerializeField]
-    LayerMask layermask;
+    LayerMask layermask, layermask2;
     MeshExcavator excavator;
     [SerializeField]
     MeshCollider colliderCopy;
@@ -16,6 +15,13 @@ public class IndicatorFollow : MonoBehaviour
     [SerializeField]
     float excavatingSpeed;
     // Start is called before the first frame update
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    void Start () {
+        excavator = gameObject.GetComponent<MeshExcavator> ();
+=======
+>>>>>>> Stashed changes
     //Aca todo las variables que agregue para que pase lo que pasa en la escena 1 :u
     [SerializeField] private List<GameObject> huesos;
     [SerializeField] private Mesh mesh;
@@ -31,10 +37,34 @@ public class IndicatorFollow : MonoBehaviour
     {
         count = 0;
         excavator = gameObject.GetComponent<MeshExcavator>();
+>>>>>>> cb43780f41c094486c77f4936b7680ec08cc8a8c
         cam = Camera.main;
     }
 
     // Update is called once per frame
+<<<<<<< HEAD
+    void Update () {
+        CursorToWorldPosition ();
+    }
+
+    void CursorToWorldPosition () {
+        Ray rayToCameraPos = cam.ScreenPointToRay (Input.mousePosition);
+        RaycastHit hitInfo;
+        RaycastHit hitInfo2;
+
+        if (Physics.Raycast (rayToCameraPos, out hitInfo, 100, layermask)) {
+            transform.position = hitInfo.point;
+            transform.rotation = Quaternion.FromToRotation (Vector3.up, hitInfo.normal);
+            if (excavator != null && hitInfo.rigidbody != null) {
+                excavatingSpeed += Input.mouseScrollDelta.y * 0.1f;
+                excavatingSpeed = Mathf.Clamp (excavatingSpeed, 0, 10);
+                bool excavating = false;
+                bool canExcavate = true;
+                excavating = Input.GetMouseButton (0);
+
+                canExcavate = excavator.PushMeshAlongVerts (hitInfo.triangleIndex, hitInfo.rigidbody.gameObject.GetComponent<MeshFilter> ().mesh, hitInfo.normal, excavatingSpeed * Time.deltaTime, hitInfo.rigidbody.gameObject.transform, colliderCopy, excavating);
+                if (excavating && canExcavate) {
+=======
     void Update()
     {
         if (count >= 4)
@@ -68,16 +98,63 @@ public class IndicatorFollow : MonoBehaviour
                 canExcavate = excavator.PushMeshAlongVerts(hitInfo.triangleIndex, hitInfo.rigidbody.gameObject.GetComponent<MeshFilter>().mesh, hitInfo.normal, excavatingSpeed * Time.deltaTime, hitInfo.rigidbody.gameObject.transform, colliderCopy, excavating);
                 if (excavating && canExcavate)
                 {
+>>>>>>> cb43780f41c094486c77f4936b7680ec08cc8a8c
                     rocks.emissionRate = 50 * (excavatingSpeed);
                     rocks.startSpeed = 30 * (excavatingSpeed / 10);
-                }
-                else
-                {
+
+                    if (Physics.Raycast (rayToCameraPos, out hitInfo2, 100, layermask2)) {
+                        //print ("es hueso");
+                        Vector3 pos = hitInfo2.transform.position;
+                        pos.y = pos.y - excavatingSpeed * Time.deltaTime*2f;
+                        hitInfo2.transform.position = pos;
+                        //excavatingSpeed * Time.deltaTime
+
+                    }
+
+                } else {
                     rocks.emissionRate = 0;
                 }
             }
         }
 
+<<<<<<< HEAD
+    }
+}
+=======
+        Vector3[] vertices = mesh.vertices;
+        if (oneTime)
+        {
+            count += 1;
+            print(count);
+            oneTime = false;
+        }
+        if (TouchElemengt)
+        {
+            print("E tocado un hueso");
+            if (!ToolsScript.instancia.CanGive)
+            {
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    float distancia = Vector3.Distance(huesos[getid].transform.position,(vertices[i]));//aca detecta distancia entre lso huesos
+
+                    if (distancia < 1f)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Mouse0))
+                        {
+                            oneTime = true;// aca si toca el hueso cuando 
+                        }
+                    
+                    }
+                }
+
+
+
+            }
+         
+
+        }
+
+<<<<<<< Updated upstream
         Vector3[] vertices = mesh.vertices;
         if (oneTime)
         {
@@ -112,15 +189,26 @@ public class IndicatorFollow : MonoBehaviour
         }
 
 
+=======
+>>>>>>> Stashed changes
+
 
     }
     public int IdHueso(int i)
     {
         return i;
     }
+<<<<<<< Updated upstream
+    public int IdHueso(int i)
+    {
+        return i;
+    }
+=======
+>>>>>>> Stashed changes
    
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position);
     } 
 }
+>>>>>>> cb43780f41c094486c77f4936b7680ec08cc8a8c
---
+++ Paleontojam_Game/Assets/Scripts/nivel_1-/detectar_huesos_libres.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectar_huesos_libres : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void OnTriggerEnter (Collider other) {
+        print ("entro  "+other.name);
+    }
+
+    public void OnTriggerExit (Collider other) {
+        print ("salio  ");
+        if (other.gameObject.layer == 12) {
+            print (other.name);
+        }
+
+    }
+
+}
